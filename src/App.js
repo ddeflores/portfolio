634e02bea0ecdd2566c21afc26ed78e02f197aaa
ddeflores <94253526+ddeflores@users.noreply.github.com>
2024-01-27 13:13:20 -0500

    add career insight section, add chess demo video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import chirp from './/chirp.png';
-import chess from './/chess.png';
+import chess from './/chess.png'
+import chessDemo from './Chess Game Demo.mov';
 import studybuddy from './/studybuddy.png';
 import linkedin from './linkedin.png';
 import github from './/github.webp';
@@ -95,22 +96,27 @@ function App() {
                         <div class="preface">
                         I'm an aspiring software engineer. 
                         </div>
-                        <div class="preface">
-                            Before I show you some of my work, here's a little bit about me.
+                        <div class="preface" style={{borderBottom: '2px solid #BFA181', paddingBottom: '20px'}}>
+                            Before I show you some of my work, here are some insights about my connection with computer science.
                         </div>
-                        <div class="paragraph" style={{paddingTop: '75px', borderTop: '2px solid #BFA181'}}>
-                        I love coffee and trying out new coffee shops whenever I go somewhere new. 
-                        It's also my favorite pre-workout for lifting or running! If you're interested,
-                        my bench PR is 265x3 and my fastest mile is 6:15! I love working to improve these numbers.
+                        <div class="paragraph" style={{paddingTop: '75px'}}>
+                            Computer science, to me, is not just a field of study but an outlet for creativity.
+                            I have always loved tinkering with the technology around me and figuring out how things work.
+                            Computer science gives me the opportunity to solve intricate problems in creative ways. It 
+                            offers the unique potential to transform the world, and my primary motivation lies in
+                            developing impactful solutions that address real-world challenges. Whether for healthcare, 
+                            environmental sustainability, or education, each line of code is a step towards a better future.
                         </div>
                         <div class="paragraph">
-                            I also love watching a good baseball game when my day is over to unwind. Before I started my career in computer science, I was hoping for a career in baseball. 
-                            Its been a passion of mine for over 10 years!
-                            Some other interests I have are playing chess and poker. Nothing beats a cool checkmate or bluffing my way to a win with 7-2 offsuit.
+                            My main career goal is to align with an organization that shares my perspective on the transformative 
+                            power of computer science.  I am deeply passionate about creating dependable software that is accessible
+                            to everyone and am particularly driven to harness AI and machine learning to foster positive change. The 
+                            potential of AI to revolutionize mental healthcare and offer life-changing support to those affected by 
+                            mental illness truly resonates with me. I would love to contribute and grow in an environment that is actively
+                            shaping solutions that heal and empower others.
                         </div>
                         <span class="conclusion">
-                            So yeah, that's me. If you actually cared to read all that, check out some of my projects{' '}
-                            <span style={{textDecoration: 'underline', cursor: 'pointer'}} onClick={handleProjectsClick}>here</span>
+                            If you've taken the time to read through everything, feel free to explore some of my projects below!
                         </span>
                         <div style={{height: '10px'}}/>
                     </div>       
@@ -118,9 +124,6 @@ function App() {
                         <div class="section">
                             <div class="introduction">
                                 Here are some of the projects I've worked on.
-                            </div>
-                            <div class="preface">
-                            (You can click on the titles/icons and check them out!)
                             </div>
                         </div>
                         <div class="projects">
@@ -132,18 +135,36 @@ function App() {
                                         </div>
                                     </a>
                                     <h3>Tech: React Native, JavaScript, Expo, Firebase, Tensorflow.js, Python, OpenAI API</h3>
-                                    <video id='videoID' poster={foodfinder} playsInline controls loop muted width="250">
+                                    <video poster={foodfinder} playsInline controls loop muted width="250">
                                         <source src={foodfinderVideo} />
                                         Your browser does not support the video tag.
                                     </video>
                                     <p style={{ marginBottom: '50px'}}>
                                     This is my most exciting project yet! I've always wanted a MyFitnessPal type app, but with a smoother, more modern UI.
-                                    Hence, why I started making FoodFinder. It's still a work in progress, but I'm super excited to keep adding more features. 
+                                    Hence, why I started making FoodFinder. It's still a work in progress, but I'm very enthusiastic about adding more features. 
                                     I eventually want to post this to the App Store and Google Play Store. The coolest feature in my opinion is the ability to
                                     upload images of foods and receive estimations of how many calories are in them. Perfect for people who aren't very well-versed
                                     in nutrition! For the time being, I don't have this deployed anywhere, but I included a demo video above. It showcases how 
-                                    users can log food by taking pictures, how users can log food manually, and a few other things like editing and deleting foods 
+                                    users can log food by taking pictures, how users can log food manually, and a few other things like editing and deleting foods in the log, 
                                     and the acount page.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="section">
+                                <div class="links">
+                                    <a href="https://eorev.github.io/Chirp/" style={{textDecoration: 'none'}}>
+                                        <div class="name">Chirp</div>
+                                    </a>
+                                    <h3>Tech: React, TypeScript, Firebase</h3>
+                                    <a href="https://eorev.github.io/Chirp/">
+                                        <img src={chirp} alt="chirp" height="200" width="220"/>
+                                    </a>
+                                    <p style={{ marginBottom: '50px'}}>
+                                    I worked on Chirp with a team for a 24 hour hackathon, HenHacks 2023, which was hosted by the University of Delaware.
+                                    Chirp was designed for students of all ages and CS backgrounds learning sorting algorithms. Our main focus was to have 
+                                    a very clean UI that would be easily accessible, even for children. My team and I achieved second place in the Educational
+                                    category for our design and efforts, out of over 150 competitors! I'm very proud of the outcome of our hard work, but regardless 
+                                    of how we did I'm glad I had such a great learning experience for my first hackathon!
                                     </p>
                                 </div>
                             </div>
@@ -157,7 +178,7 @@ function App() {
                                         <img src={posterImg} alt="neural network research" height="400" width="300"/>
                                     </a>
                                     <p style={{ marginBottom: '50px'}}>
-                                        This is an outline of research my peer and I conducted. We compared convolutional neural networks (CNN) to recurrent
+                                        This is some research my peer and I conducted. We compared convolutional neural networks (CNN) to recurrent
                                         neural networks (RNN) in their effectiveness classifying URLs as phishing or non-phishing. We trained each
                                         model on a dataset of over 800,000 URLs, and eventually concluded that CNNs are more effective when 
                                         processing time is taken into account. It proved to be a great learning experience, and it also sparked my interest
@@ -190,25 +211,7 @@ function App() {
                                     make a file storage/sharing app similar to Google Drive.
                                     With StudyBuddy, users can either choose any file from their system, or make a text file inside of the app.
                                     They can also share these files with people they add as friends, via user ID.
-                                    This is a pretty bare-bones application, but it's pretty cool to look back on this and see how far I've come!
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="section">
-                                <div class="links">
-                                    <a href="https://eorev.github.io/Chirp/" style={{textDecoration: 'none'}}>
-                                        <div class="name">Chirp</div>
-                                    </a>
-                                    <h3>Tech: React, TypeScript, Firebase</h3>
-                                    <a href="https://eorev.github.io/Chirp/">
-                                        <img src={chirp} alt="chirp" height="200" width="220"/>
-                                    </a>
-                                    <p style={{ marginBottom: '50px'}}>
-                                    I worked on Chirp with a team for a 24 hour hackathon, HenHacks 2023, which was hosted by the University of Delaware.
-                                    Chirp was designed for students of all ages and CS backgrounds learning sorting algorithms. Our main focus was to have 
-                                    a very clean UI that would be easily accessible, even for children. My team and I achieved second place in the Educational
-                                    category for our design and efforts, out of over 150 competitors! I'm very proud of the outcome of our hard work, but regardless 
-                                    of how we did I'm glad I had such a great learning experience for my first hackathon!
+                                    It's really neat to look back on this and see how far I've come!
                                     </p>
                                 </div>
                             </div>
@@ -220,15 +223,34 @@ function App() {
                                         </div>
                                     </a>
                                     <h3>Tech: Java, Java Swing</h3>
-                                    <a href="https://github.com/ddeflores/ChessGame">
-                                        <img src={chess} alt="chessgame" height="200" width="200"/>
-                                    </a>
+                                    <video poster={chess} playsInline controls loop muted width="250">
+                                        <source src={chessDemo}/>
+                                        Your browser does not support the video tag.
+                                    </video>
                                     <p style={{ marginBottom: '50px'}}>
                                     I wanted to practice my Object Oriented Programming skills and figured Chess was a great
-                                    starting point, especially since I'm quite passionate about it! Unfortunately I don't have
-                                    this deployed anywhere, but I included a screenshot of the GUI that I made.
-                                    </p>
+                                    starting point, especially since I'm quite passionate about it! In this program, I implemented 
+                                    the classic rules of chess. If you want to see how it works, I included a short demo video of some gameplay
+                                    with the GUI that I made.
+                                    </p>                                    
                                 </div>
+                            </div>
+                        </div>
+                        <div class="section">
+                            <div class="name" style={{paddingTop: '75px'}}>
+                                Now, here are some extras about me!
+                            </div>
+                            <div class="paragraph">
+                            I love coffee and trying out new coffee shops whenever I go somewhere new. 
+                            It's also my favorite pre-workout for lifting or running!
+                            </div>
+                            <div class="paragraph">
+                                Before I started my career in computer science, I was hoping for a career in baseball. 
+                                Its been a passion of mine for over 10 years, and its nice to watch a game here and there
+                                to relax. My favorite team is the New York Mets!
+                            </div>
+                            <div class="paragraph" style={{paddingBottom:'75px', borderBottom: '2px solid #BFA181'}}>
+                            I also love playing chess and poker. Nothing beats a cool checkmate or bluffing my way to a win with 7-2 offsuit!
                             </div>
                         </div>
                         <div class="section">
